72091c122885b70d5f398685f2e3566e76d56a8d
ibrahimdevop21 <ibrahimdevop21@gmail.com>
2025-01-29 21:53:21 +0000

    feat: Configure project for Vercel deployment
    This commit configures the project for deployment on Vercel by:
    
    - Removing the Netlify adapter from `astro.config.mjs`.
    - Updating `package.json` to remove the Netlify adapter dependency.
    - Updating `package.json` start script
    - Changing the output in `astro.config.mjs` from server to static
    - Deleting the netlify.toml file.
    - Removing unused  Astro component
    - Enhancing the UI on TrustedBy and Hero component.
    - Adding Testimonials component.
    
    These changes ensure the project is optimized for Vercel's environment and builds correctly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: 'server',
-  adapter: netlify(),
+  output: 'static',
 });
